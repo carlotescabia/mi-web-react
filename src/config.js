ef686416367fe01b215a4bd82c3ddaec14d1562b
carlotescabia <carlotaescabiariera@gmail.com>
2025-08-15 01:03:45 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+```javascript
+// src/config.js
+export const GOOGLE_SHEETS_CONFIG = {
+API_KEY: 'AIzaSyC9VLEN2SF6vehEQUsXrla4RFP9gip1D0M',
+SPREADSHEET_ID: '19irsOwQBb_L5btJedHxkTt-BsdsOsKsV7mK0euEmBIM',
+RANGE: 'Hoja 1!A:D' // O el nombre que le hayas puesto
+};
+```
